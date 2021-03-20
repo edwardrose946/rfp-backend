c58a79787aad421d38cff76029e67140611a3be8
Edward Rose <edwardrose1234@gmail.com>
2021-03-20 02:52:50 +0000

    env index

--- index.ts
+++ index.ts
@@ -1,24 +1,42 @@
 /* eslint-disable no-console */
 import mongoose, { Error } from 'mongoose';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import { apolloServer } from './server';
 import config from './utils/config';
 import cors from 'cors';
 import express from 'express';
 import { parseAsString } from './utils/type-guards';
 
+const client = new SecretManagerServiceClient();
 
-console.error(config.SECRET_JWT);
-console.error(config.GOOGLE_MAPS_API_KEY);
-console.error(config.EPC_API_KEY);
-console.error(config.MONGODB_URI);
+async function getSecret(name: string): Promise<string> {
 
-export const MONGODB_URI = parseAsString(config.MONGODB_URI);
-export const SECRET_JWT = parseAsString(config.SECRET_JWT);
-export const GOOGLE_MAPS_API_KEY = parseAsString(config.GOOGLE_MAPS_API_KEY);
-export const PROPERTY_DATA_API_KEY = parseAsString(config.PROPERTY_DATA_API_KEY);
-export const EPC_API_KEY = parseAsString(config.EPC_API_KEY);
+    const [accessResponse] = await client.accessSecretVersion({
+        name: name
+    });
 
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return accessResponse.payload.data.toString('utf8');
+}
 
+async function setSecrets() {
+    EPC_API_KEY = await getSecret('projects/326578430699/secrets/EPC_API_KEY/versions/1');
+    SECRET_JWT = await getSecret('projects/326578430699/secrets/JWT_SECRET/versions/1');
+    GOOGLE_MAPS_API_KEY = await getSecret('projects/326578430699/secrets/GOOGLE_MAPS_API_KEY/versions/1');
+    MONGODB_URI = await getSecret('projects/326578430699/secrets/MONGODB_URI/versions/1');
+    PROPERTY_DATA_API_KEY = await getSecret('projects/326578430699/secrets/PROPERTYDATA_API_KEY/versions/1');
+}
+
+export let MONGODB_URI = parseAsString(config.MONGODB_URI);
+export let SECRET_JWT = parseAsString(config.SECRET_JWT);
+export let GOOGLE_MAPS_API_KEY = parseAsString(config.GOOGLE_MAPS_API_KEY);
+export let PROPERTY_DATA_API_KEY = parseAsString(config.PROPERTY_DATA_API_KEY);
+export let EPC_API_KEY = parseAsString(config.EPC_API_KEY);
+
+setSecrets()
+    .then(()=> console.log('set'))
+    .catch(err => console.error(err));
 
 mongoose.connect(MONGODB_URI, {
     useCreateIndex: true,
